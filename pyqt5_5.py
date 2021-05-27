c0f0ef9b3a03a385465eab151684589fb3d21289
Oheuntaek3563 <jerry1725@naver.com>
2021-05-27 14:54:27 +0900

    pyqt5공부

--- pyqt5_5.py
+++ pyqt5_5.py
@@ -10,7 +10,7 @@ class MyApp(QMainWindow):
         self.initUI()
 
     def initUI(self):
-        self.statusBar().showMessage('※공부중~~')
+        self.statusBar().showMessage('※공부중~~') # showMessage()를 사용, 상태바에 표시될 메세지 설정 | QMainWindow클래스의 statusbar로 상태바 생성 
         
         self.setWindowTitle('Statusbar')
         self.setGeometry(300, 300, 300, 200)
