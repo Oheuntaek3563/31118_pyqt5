2086e2a7e80f7f5bbcad4a0df239c0c8e48c6e4e
Oheuntaek3563 <jerry1725@naver.com>
2021-05-27 13:46:08 +0900

    pyqt5_창띄우기

---
+++ pyqt5_2.py
@@ -0,0 +1,24 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
+
+""" pyqt5 어플리케이션 아이콘 삽입"""
+
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('Icon') 
+        self.setWindowIcon(QIcon('web.png')) # setWindowIcon() : 어플리케이션 아이콘을 설정함.  
+        self.setGeometry(300, 300, 300, 200) # setGeometry() : 창의 위치와 크기를 설정. 
+        """ 첫번째와 두번째는 x,y의 위치를 지정, 세번째와 네번째는 창의 너비와 높이를 결정 """
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
