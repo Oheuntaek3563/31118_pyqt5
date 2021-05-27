b0f2266543fbde4a77e48e528cc23186454622c9
Oheuntaek3563 <jerry1725@naver.com>
2021-05-27 14:17:58 +0900

    pyqt5_20210527

--- pyqt5_2.py
+++ pyqt5_2.py
@@ -2,7 +2,7 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5.QtGui import QIcon
 
-""" pyqt5 어플리케이션 아이콘 삽입"""
+""" pyqt5 어플리케이션 아이콘 삽입하기"""
 
 class MyApp(QWidget):
 
@@ -12,7 +12,7 @@ class MyApp(QWidget):
 
     def initUI(self):
         self.setWindowTitle('Icon') 
-        self.setWindowIcon(QIcon('web.png')) # setWindowIcon() : 어플리케이션 아이콘을 설정함.  
+        self.setWindowIcon(QIcon('web.png')) # setWindowIcon() : 어플리케이션 아이콘을 설정함. 
         self.setGeometry(300, 300, 300, 200) # setGeometry() : 창의 위치와 크기를 설정. 
         """ 첫번째와 두번째는 x,y의 위치를 지정, 세번째와 네번째는 창의 너비와 높이를 결정 """
         self.show()
